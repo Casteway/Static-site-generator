b915ec1e5488bb83e4cd9817fef09bc4c4408ac6
Krystian <napier2211@gmail.com>
2025-12-05 10:18:22 +0100

    added LeafNode with unittests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,4 +1,4 @@
-class HtmlNode():
+class HTMLNode():
     def __init__(self,tag=None,value=None,children=None,props=None):
         self.tag = tag
         self.value = value
@@ -18,3 +18,14 @@ class HtmlNode():
 
     def __repr__(self):
         return f"{self.tag}, {self.value}, {self.children}, {self.props}"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,25 +1,37 @@
 import unittest
-from htmlnode import HtmlNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_to_html_props(self):
-        node = HtmlNode(props={"href": "https://www.google.com", "target": "_blank"})
+        node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
         expected = ' href="https://www.google.com" target="_blank"'
         actual = node.props_to_html()
         self.assertEqual(expected, actual)
 
     def test_values(self):
-        node = HtmlNode()
+        node = HTMLNode()
         expected = ""
         actual = node.props_to_html()
         self.assertEqual(expected, actual)
     
     def test_storage(self):
-        node = HtmlNode(tag="div", value="I am a div")
+        node = HTMLNode(tag="div", value="I am a div")
         self.assertEqual(node.tag, "div")
         self.assertEqual(node.value, "I am a div")
         self.assertEqual(node.children, None)
         self.assertEqual(node.props, None)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+    
+    def test_leaf_raw_text(self):
+        node = LeafNode(None, "Just raw text")
+        self.assertEqual(node.to_html(), "Just raw text")
         
 if __name__ == "__main__":
     unittest.main()
